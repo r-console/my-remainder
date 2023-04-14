09e89a9c33adde11b5322ebd0375bd9a8e2e3509
r-console <rameshsolaimalai878@gmail.com>
2023-04-14 17:45:06 +0530

    app version

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/myremainder/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/myreminder/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
